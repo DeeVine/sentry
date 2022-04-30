4b8177d376aaaa2192dc92ba652dfa62f63c4bf7
darren <kwongdarren@gmail.com>
2022-04-30 16:25:53 -0700

    commit of sentry test project

---
+++ sentry.js
@@ -0,0 +1,31 @@
+const Sentry = require("@sentry/node");
+// or use es6 import statements
+// import * as Sentry from '@sentry/node';
+
+const Tracing = require("@sentry/tracing");
+// or use es6 import statements
+// import * as Tracing from '@sentry/tracing';
+
+Sentry.init({
+  dsn: "https://4eedf6f3a0fb4489aa726bda00dae44f@o1225866.ingest.sentry.io/6371595",
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+const transaction = Sentry.startTransaction({
+  op: "test",
+  name: "My First Test Transaction",
+});
+
+setTimeout(() => {
+  try {
+    foo();
+  } catch (e) {
+    Sentry.captureException(e);
+  } finally {
+    transaction.finish();
+  }
+}, 99);
